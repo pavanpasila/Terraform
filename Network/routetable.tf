46006a76e632e64965f6713008ba25bfe4a67f98
pavanpasila <65785317+pavanpasila@users.noreply.github.com>
2021-08-26 11:48:27 +0100

    Update routetable.tf

--- Network/routetable.tf
+++ Network/routetable.tf
@@ -12,3 +12,8 @@ resource "aws_route_table" "prod-public-crt" {
         Name = "prod-public-crt"
     }
 }
+
+resource "aws_route_table_association" "prod-crta-public-subnet-1"{
+    subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
+    route_table_id = "${aws_route_table.prod-public-crt.id}"
+}
