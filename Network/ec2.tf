4fad285ba6dfade1b26ea0bf15ff1f3dd597345c
pavanpasila <65785317+pavanpasila@users.noreply.github.com>
2021-08-28 13:56:27 +0100

    Update ec2.tf

--- Network/ec2.tf
+++ Network/ec2.tf
@@ -1,30 +1,38 @@
 resource "aws_instance" "web1" {
+
     ami = "${lookup(var.AMI, var.AWS_REGION)}"
     instance_type = "t2.micro"
+
     # VPC
     subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
+
     # Security Group
     vpc_security_group_ids = ["${aws_security_group.ssh-allowed.id}"]
+
     # the Public SSH key
-    key_name = "${aws_key_pair.ohio-region-key-pair.id}"
-    # nginx installation
+    key_name = "${aws_key_pair.london-region-key-pair.id}"
+
+    # nginx installation
     provisioner "file" {
         source = "nginx.sh"
         destination = "/tmp/nginx.sh"
     }
+
     provisioner "remote-exec" {
         inline = [
-             "chmod +x /tmp/nginx.sh",
-             "sudo /tmp/nginx.sh"
+            "chmod +x /tmp/nginx.sh",
+            "sudo /tmp/nginx.sh"
         ]
     }
+
     connection {
         user = "${var.EC2_USER}"
+        host = "${aws_instance.web1.public_ip}"
         private_key = "${file("${var.PRIVATE_KEY_PATH}")}"
     }
 }
-// Sends your public key to the instance
-resource "aws_key_pair" "ohio-region-key-pair" {
-    key_name = "ohio-region-key-pair"
+
+resource "aws_key_pair" "london-region-key-pair" {
+    key_name = "london-region-key-pair"
     public_key = "${file(var.PUBLIC_KEY_PATH)}"
 }
