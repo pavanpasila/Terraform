0cc0748aca43c3ece4673e195e713beaa78cc1ab
pavanpasila <65785317+pavanpasila@users.noreply.github.com>
2021-08-28 15:23:48 +0100

    Update var.tf

--- Network/var.tf
+++ Network/var.tf
@@ -17,8 +17,7 @@ variable "AMI" {
   type = map
 
   default =  {
-    eu-west-2 = "ami-03dea29b0216a1e03"
-    us-east-1 = "ami-0c2a1acae6667e438"
+    eu-west-2 = "ami-0194c3e07668a7e36"
   }
 }
 
