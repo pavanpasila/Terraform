636031e360e94059db43dd5d62b44c9ed6b81184
pavanpasila <65785317+pavanpasila@users.noreply.github.com>
2021-08-28 13:54:06 +0100

    Update var.tf

--- Network/var.tf
+++ Network/var.tf
@@ -1,26 +1,27 @@
-variable "AWS_REGION" {    
-    default = "us-east-2"
-}
-
-variable "AMI" {
-    type = "map"
-    
-    default {
-        us-east-2 = "ami-0443305dabd4be2bc"
-    }
+variable "AWS_REGION" {
+  default = "eu-west-2"
 }
 
 variable "PRIVATE_KEY_PATH" {
-  default = "ohio-region-key-pair"
+  default = "london-region-key-pair"
 }
 
 variable "PUBLIC_KEY_PATH" {
-  default = "ohio-region-key-pair.pub"
+  default = "london-region-key-pair.pub"
 }
 
 variable "EC2_USER" {
   default = "ubuntu"
 }
+variable "AMI" {
+  type = map
+
+  default =  {
+    eu-west-2 = "ami-03dea29b0216a1e03"
+    us-east-1 = "ami-0c2a1acae6667e438"
+  }
+}
+
 
 
 
