29441bc790a0cc0d78deca2c9a608f05ad1596bc
pavanpasila <65785317+pavanpasila@users.noreply.github.com>
2021-08-30 13:10:14 +0100

    Update nginx.sh

--- Network/nginx.sh
+++ Network/nginx.sh
@@ -5,9 +5,9 @@ until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
   sleep 1
 done
 
-# install nginx
-# apt-get update
+# install apache
+apt update -y
 apt install apache2 -y
 
 # make sure nginx is started
-apt start apache2
+service apache2 start
